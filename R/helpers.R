9152402e2bc700b990519303e071fe44bd7f053f
Elio Campitelli <elio.campitelli@pikachu.cima.fcen.uba.ar>
2020-02-18 13:06:04 -0300

    Ahora sí arregla los conflilctos

--- R/helpers.R
+++ R/helpers.R
@@ -8,11 +8,7 @@
 }
 
 
-<<<<<<< HEAD
-.pattern_python_to_r <- function(pattern) {
-=======
 .pattern_python_to_r <- function(x) {
->>>>>>> 40df7796b850ef3f41539b2e8a1ad95fbb200c73
   x <- gsub("%(", "{", x, fixed = TRUE)
   x <- gsub(")s", "}", x, fixed = TRUE)
   x
